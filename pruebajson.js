9b7aec495c04c58c683356bda769b416b5ce5b3d
unknown <agomezseguros@gmail.com>
2024-06-02 21:15:14 -0300

    cambios angel2

--- pruebajson.js
+++ pruebajson.js
@@ -19,6 +19,14 @@ copia.style.padding = '15px';
 Contenedor.appendChild(copia.cloneNode(true));
 Contenedor.appendChild(copia.cloneNode(true));
 
+fetch("https://Sacv2.github.io/grupo4/datagames.json")
+.then(response => response.json())
+.then(data => {
+    // Procesamiento de la info que llega de la API
+    console.log(data);
+    })
+.catch(error => console.log("Ocurrió un error! " + error));
+
 
 
 
