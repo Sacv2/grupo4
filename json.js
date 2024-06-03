f30ca0b885a9d560550330d06b820eef6b6d17e6
unknown <agomezseguros@gmail.com>
2024-06-03 13:24:32 -0300

    arreglo json 3

--- json.js
+++ json.js
@@ -2,7 +2,7 @@ let FC24 = document.querySelector("#FC24");
 
 let referencia = FC24.cloneNode(true);
 
-
+/*
 fetch("https://Sacv2.github.io/grupo4/datagames.json")
 .then(response => response.json())
 .then(data => {
@@ -11,10 +11,10 @@ fetch("https://Sacv2.github.io/grupo4/datagames.json")
 
     console.log(data[0].nombre + " " + data[0].precio);
 
-    FC24.querySelector("img").src = data[0].imagen
-    FC24.querySelector("img").alt = "FC24"
-    FC24.querySelector("h3").innerHTML = data[0].nombre
-    FC24.querySelector("p").innerHTML = data[0].precio
+    FC24.querySelector("img").src = data[0].imagen;
+    FC24.querySelector("img").alt = "FC24";
+    FC24.querySelector("h3").innerHTML = data[0].nombre;
+    FC24.querySelector("p").innerHTML = data[0].precio;
 
     })
 .catch(error => console.log("Ocurrió un error! " + error));
